c155552a41200ed064ad47347091ba299deef849
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-08-23 21:13:53 +0200

    Selektol som 4 classy v HTML a vytvoril 2 addlistenery.

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-let bigText = document.querySelector("#big-text"),
-    litleText = document.querySelector("#litle-text");
+let bigText       = document.querySelector("#big-text"),
+    litleText     = document.querySelector("#litle-text"),
+    preview       = document.querySelector(".preview"),
+    previewSmall  = document.querySelector(".preview2");
+
+    console.log( previewSmall )
+
+
+bigText.addEventListener("input", function(e) {
+    e.preventDefault();
+
+    preview.textContent =  e.target.value 
+
+});
+
+litleText.addEventListener("input", function(e) {
+    e.preventDefault();
+
+    // previewSmall.textContent =  e.target.value 
+    previewSmall.innerText = e.target.value 
+
+});
+
+
