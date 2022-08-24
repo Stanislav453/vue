6c971e13376463a43f9fe436beb1ba4542b37402
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-08-24 12:38:38 +0200

    Odstranil som a zakomentovla prebytocny kod, selectol som s DOM .miror.

--- script.js
+++ script.js
@@ -2,32 +2,38 @@ let bigText = document.querySelector("#big-text"),
   litleText = document.querySelector("#litle-text"),
   preview = document.querySelector(".preview"),
   previewSmall = document.querySelector(".preview2"),
-  appBoddy = document.querySelector("#app");
+  appBoddy = document.querySelector("#app"),
+  divMiror = document.querySelector(".mirror");
 
-appBoddy.addEventListener("keydown", function (e) {
-  console.log(e);
-  if ((e.data = "Enter")) {
-    console.log("Ahoj");
-  }
+  console.log( divMiror )
+
+// appBoddy.addEventListener("keydown", function (e) {
+//   console.log(e);
+//   if ((e.data = "Enter")) {
+//     console.log("Ahoj");
+//   }
+// });
+
+bigText.addEventListener("input", function (bigText) {
+
+  let myBigTextValue = bigText.target.value;
+
+  preview.textContent = myBigTextValue;
+
 });
 
-// bigText.addEventListener("keyup", function (e) {
-
-//   var myBigText = (preview.textContent = e.target.value);
-
-// });
-
-// litleText.addEventListener("input", function (e) {
-//   previewSmall.innerText = e.target.value;
-// });
+litleText.addEventListener("input", function (e) {
+  previewSmall.innerText = e.target.value;
+});
 
 // document.body.addEventListener("keyup", function(e) {
 //     if( e.key === "Enter" ) {
-//       console.log("Ahoj")
-//     }
-
+      
 //     let catchUl  = document.querySelector("ul")
 //     let fragment = document.createDocumentFragment();
 //     let li = fragment.appendChild(document.createElement("li"));
-//     li.textContent = e.target.value;
+//     li.textContent = myBigTextValue;
+
+//     catchUl.appendChild(fragment)
+//     }
 // })
