4a5e6ceda1476e0923ecc8244c7184f3f578319f
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-08-25 17:10:53 +0200

    Upravil som kód, Vytvoril šíkovú notáciu, zabalil pridanie blocku do funckie

--- script.js
+++ script.js
@@ -1,49 +1,43 @@
-let bigText = document.querySelector("#big-text"),
-  litleText = document.querySelector("#litle-text"),
-  preview = document.querySelector(".preview"),
-  previewSmall = document.querySelector(".preview2"),
-  appBoddy = document.querySelector("#app"),
-  mirror   = document.querySelector( ".mirror" );
+let bigText      = document.querySelector("#big-text"),
+    litleText    = document.querySelector("#litle-text"),
+    preview      = document.querySelector(".preview"),
+    previewSmall = document.querySelector(".preview2"),
+    appBoddy     = document.querySelector("#app"),
+    mirror       = document.querySelector(".mirror");
+
+  //Zrkadlenie textu s inputu bigtext/litleText
+bigText.addEventListener("input", (e) => preview.textContent = e.target.value);
+
+litleText.addEventListener("input", (e) => previewSmall.textContent = e.target.value);
 
 
-// appBoddy.addEventListener("keydown", function (e) {
-//   console.log(e);
-//   if ((e.data = "Enter")) {
-//     console.log("Ahoj");
-//   }
-// });
+//pridávanie odstavcov
+document.body.addEventListener("keyup", function (e) {
 
-bigText.addEventListener("input", function (e) {
-  preview.textContent = e.target.value;
+  if (e.key === "Enter") {
 
+    addBlock();
+
+  }
 });
 
-litleText.addEventListener("input", function (e) {
-  previewSmall.textContent = e.target.value;
-});
-
-console.log( preview.innerText )
-
-document.body.addEventListener("keyup", function(e) {
-    if( e.key === "Enter" ) {
-
-    
-      
-    let catchUl  = document.querySelector("ul")
-    let fragment = document.createDocumentFragment();
-    let article= fragment
-            .appendChild(document.createElement("li"))
-            .appendChild(document.createElement("article"))
-      article.classList.add("dude")
-    article.textContent = preview.textContent;
-
-    let spanText = document.createElement("span")
-    spanText.textContent = previewSmall.textContent
-
-    article.appendChild(spanText)
-    catchUl.appendChild(fragment)
 
 
-    }
-})
 
+// Block textu
+function addBlock () {
+
+  let catchUl = document.querySelector("ul");
+  let fragment = document.createDocumentFragment();
+  let article = fragment
+    .appendChild(document.createElement("li"))
+    .appendChild(document.createElement("article"));
+  article.classList.add("dude");
+  article.textContent = preview.textContent;
+
+  let spanText = document.createElement("span");
+  spanText.textContent = previewSmall.textContent;
+
+  article.appendChild(spanText);
+  catchUl.appendChild(fragment);
+};
