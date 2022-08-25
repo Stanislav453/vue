38b0015ba59510ca9e3c1ff466abae42be514d0f
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-08-25 17:30:01 +0200

    Upravil som kód,vytvoril komenty, pridal addBlock().

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// General
+let bigText      = document.querySelector("#big-text"),
+    litleText    = document.querySelector("#litle-text"),
+    preview      = document.querySelector(".preview"),
+    previewSmall = document.querySelector(".preview2"),
+    appBoddy     = document.querySelector("#app"),
+    mirror       = document.querySelector(".mirror");
+
+//Zrkadlenie textu s inputu bigtext/litleText
+bigText.addEventListener("input", (e) => preview.textContent = e.target.value);
+
+litleText.addEventListener("input", (e) => previewSmall.textContent = e.target.value);
+
+
+//pridávanie odstavcov
+document.body.addEventListener("keyup", function (e) {
+
+  if (e.key === "Enter") {
+
+    app.addBlock();
+
+  }
+});
