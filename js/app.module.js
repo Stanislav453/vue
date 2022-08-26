d5abc5aa7024cafea234d6da885c5c72ca45e84e
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-08-26 11:27:25 +0200

    Vytvoril som funkciu addLi, addvoice pripol addli() k addblock()

--- js/app.module.js
+++ js/app.module.js
@@ -1,20 +1,49 @@
 var app = (function () {
 
+  var catchUl = document.querySelector("ul");
+
+
+//Vytvorenie li
+var addLi = function () {
+    var appLi =  document.createElement("li")
+        catchUl.appendChild(appLi)
+        
+        return {
+          appLi: appLi
+        }
+      };
+    
+
+//Vytvorenie počítadla (input/number)
+
+var addVoice = function () {
+     let voiceNumber =  document.createElement("input")
+         voiceNumber.setAttribute("type", "number")
+         voiceNumber.classList.add("ctrl")
+
+         return {
+            voiceNumber: voiceNumber
+         }
+}
 
 
 // Block textu
 function addBlock () {
 
-    let catchUl = document.querySelector("ul");
+    addLi();
+
     let fragment = document.createDocumentFragment();
     let article = fragment
-      .appendChild(document.createElement("li"))
       .appendChild(document.createElement("article"));
     article.classList.add("dude");
     article.textContent = preview.textContent;
+
+    addLi().appLi.appendChild(article)
+      
   
     let spanText = document.createElement("span");
     spanText.textContent = previewSmall.textContent;
+
   
     article.appendChild(spanText);
     catchUl.appendChild(fragment);
