f0cd0b25dc04608dc751c5f11499313e87c4d109
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-08-26 11:42:38 +0200

    Vytvoril som funkciu addLi()

--- js/app.module.js
+++ js/app.module.js
@@ -1,51 +1,61 @@
 var app = (function () {
 
+  //General
   var catchUl = document.querySelector("ul");
 
-
-//Vytvorenie li
-var addLi = function () {
-    var appLi =  document.createElement("li")
-        catchUl.appendChild(appLi)
-        
-        return {
-          appLi: appLi
-        }
-      };
     
 
-//Vytvorenie počítadla (input/number)
-var addVoice = function () {
+  //Vytvorenie počítadla (input/number)
+  var addVoice = function () {
      let voiceNumber =  document.createElement("input")
          voiceNumber.setAttribute("type", "number")
          voiceNumber.classList.add("ctrl")
+         voiceNumber.value = 0
+
+        //  if( voiceNumber >= 15 ) {
+
+        //  }
 
          return {
             voiceNumber: voiceNumber
          }
-}
+  }
+
+  //Vytvorenie li
+  var addLi = function () {
+    var appLi =  document.createElement("li")
+        catchUl.appendChild(appLi)
+
+        appLi.appendChild(addVoice().voiceNumber)
+      
+      
+        return {
+        appLi: appLi
+
+        }
+  };
 
 
-// Block textu
-function addBlock () {
+  // Block textu
+  function addBlock () {
 
-    let fragment = document.createDocumentFragment();
-    let article = fragment
-      .appendChild(document.createElement("article"));
-    article.classList.add("dude");
-    article.textContent = preview.textContent;
+      let fragment = document.createDocumentFragment();
+      let article = fragment
+        .appendChild(document.createElement("article"));
+        article.classList.add("dude");
+        article.textContent = preview.textContent;
 
-    addLi().appLi.appendChild(article)
-    // addLi().appLi.appendChild(addVoice())
+      addLi().appLi.appendChild(article)
       
   
-    let spanText = document.createElement("span");
-    spanText.textContent = previewSmall.textContent;
+      let spanText = document.createElement("span");
+      spanText.textContent = previewSmall.textContent;
 
-    article.appendChild(spanText);
+      article.appendChild(spanText);
 
   };
 
+  // Vracanie funkcii
   return {
 
     addBlock: addBlock
