31e42773d7de92a31987057cb6231e2cb4230a0f
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-08-28 15:31:02 +0200

    Spravil som kompletny refaktoring

--- js/app.module.js
+++ js/app.module.js
@@ -3,14 +3,41 @@ var app = (function () {
   //General
   var catchUl = document.querySelector("ul");
 
+
+  var createBlock = function() {
+
+    let fragment = document.createDocumentFragment();
+    let article = fragment
+      .appendChild(document.createElement("article"));
+      article.classList.add("dude");
+      article.textContent = preview.textContent;
+  
+
+    let spanText = document.createElement("span");
+    spanText.textContent = previewSmall.textContent;
+
+    article.appendChild(spanText);
+
+
+
+    return {
+      article: article
+
+    }
+  }
+
     
 
-  //Vytvorenie počítadla (input/number)
+  // Vytvorenie počítadla (input/number)
   var addVoice = function () {
      let voiceNumber =  document.createElement("input")
          voiceNumber.setAttribute("type", "number")
          voiceNumber.setAttribute("value", "0")
          voiceNumber.classList.add("ctrl");
+
+         voiceNumber.addEventListener("input", function(e) {
+          createBlock().article.classList.toggle("gold", e.target.value > 2)
+        })
 
          return {
             voiceNumber: voiceNumber
@@ -18,72 +45,48 @@ var app = (function () {
 
   }
 
-  
-  // //Vytvorenie remove (input/number)
-  // var addRemove = function () {
-  //   let linkRemove =  document.createElement("a")
-  //       linkRemove.classList.add("ctrl");
-  //       linkRemove.textContent = "X";
-
-  //       linkRemove.addEventListener("click", function() {
-  //         addLi().appLi.remove()
-  //         console.log( "ahoj" )
-  //       })
-
-  //       return {
-  //          linkRemove: linkRemove
-  //       }
-
-//  }
 
      //Vytvorenie li
      var addLi = function () {
       var appLi =  document.createElement("li")
           catchUl.appendChild(appLi)
-  
-          // appLi.appendChild(addRemove().linkRemove)
+
+          appLi.appendChild(createBlock().article)
           appLi.appendChild(addVoice().voiceNumber)
-        
-        
-          return {
-          appLi: appLi
   
-          }
+        
+      
     };
 
-
-
-  // Block textu
-  function addBlock () {
-
-      let fragment = document.createDocumentFragment();
-      let article = fragment
-        .appendChild(document.createElement("article"));
-        article.classList.add("dude");
-        article.textContent = preview.textContent;
-      
-
-        addLi().appLi.appendChild(article)
-  
-      let spanText = document.createElement("span");
-      spanText.textContent = previewSmall.textContent;
-
-      article.appendChild(spanText);
-
-      return {
-        article: article
-      }
-
-
-  };
 
 
 
   // Vracanie funkcii
   return {
 
-    addBlock: addBlock,
+    addLi: addLi
 
   }
     
 }());
+
+
+
+
+  
+//   //Vytvorenie remove (input/number)
+//   var addRemove = function () {
+//     let linkRemove =  document.createElement("a")
+//         linkRemove.classList.add("ctrl");
+//         linkRemove.textContent = "X";
+
+//         linkRemove.addEventListener("click", function() {
+//           addLi().appLi.remove()
+//           console.log( "ahoj" )
+//         })
+
+//         return {
+//            linkRemove: linkRemove
+//         }
+
+//  }
